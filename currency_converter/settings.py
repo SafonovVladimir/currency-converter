5adeb4fee5452e2ba7b36ed53847d67c1895b62e
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2022-06-20 21:29:08 +0300

    0.0.4

--- currency_converter/settings.py
+++ currency_converter/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os.path
 import os
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'currency_converter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-db_from_env = dj_database_url.config()
+# db_from_env = dj_database_url.config()
 # DATABASE['default'].update(db_from_env)
 DATABASES = {
     'default': {
